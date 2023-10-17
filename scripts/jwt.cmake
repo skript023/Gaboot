ca8a9549496ade6eac3c790bf04086e0e68cb0f7
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-10-17 16:40:38 +0700

    add jwt library

---
+++ scripts/jwt.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+
+set(JWT_BUILD_EXAMPLES OFF)
+set(JWT_BUILD_TESTS OFF)
+
+FetchContent_Declare(
+    jwt
+    GIT_REPOSITORY https://github.com/Thalhammer/jwt-cpp.git
+    GIT_TAG ce1f9df3a9f861d136d6f0c93a6f811c364d1d3d
+    GIT_PROGRESS TRUE
+)
+
+FetchContent_MakeAvailable(jwt)
+message("jwt module")
