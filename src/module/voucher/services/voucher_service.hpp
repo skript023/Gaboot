478530595042a283b5ed3d988372f8fb6294a70e
Fatur Muhammad <fatur_muhammad@bk.ru>
2023-12-08 22:28:07 +0700

    crate declaration of vouchers

--- src/module/voucher/services/voucher_service.hpp
+++ src/module/voucher/services/voucher_service.hpp
@@ -11,6 +11,16 @@ namespace gaboot
 		explicit voucher_service() = default;
 		~voucher_service() noexcept = default;
 
+		voucher_service(voucher_service const&) = delete;
+		voucher_service& operator=(voucher_service const&) = delete;
 
+		voucher_service(voucher_service&&) = delete;
+		voucher_service& operator=(voucher_service&&) = delete;
+
+		HttpResponsePtr create(HttpRequestPtr const& req);
+		HttpResponsePtr findAll(HttpRequestPtr const& req);
+		HttpResponsePtr findOne(HttpRequestPtr const& req, std::string const& id);
+		HttpResponsePtr update(HttpRequestPtr const& req, std::string const& id);
+		HttpResponsePtr remove(HttpRequestPtr const& req, std::string const& id);
 	};
 }
