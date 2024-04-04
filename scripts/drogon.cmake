a79127257373879861ecaeb59dcf8ba0cc00d00f
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-04-04 22:49:22 +0700

    changed to VCPKG Folder

--- scripts/drogon.cmake
+++ scripts/drogon.cmake
@@ -2,9 +2,9 @@ message("\nInstalling drogon library")
 
 if (MSVC)
     # Set the path to vcpkg for Windows
-    set(VCPKG_EXECUTABLE "$ENV{VCPKG_ROOT}/vcpkg.exe")
+    set(VCPKG_EXECUTABLE "$ENV{VCPKG_FOLDER}/vcpkg.exe")
 else()
-    set(VCPKG_EXECUTABLE "$ENV{VCPKG_ROOT}/vcpkg")
+    set(VCPKG_EXECUTABLE "$ENV{VCPKG_FOLDER}/vcpkg")
     message("GCC is used")
 endif()
 
