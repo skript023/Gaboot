aa84a7bac5be9307c53a2db2d9036d2bda412efb
Fatur Muhammad <fatur_muhammad@bk.ru>
2024-04-05 11:43:06 +0700

    change CMake

--- scripts/jsonvalidator.cmake
+++ scripts/jsonvalidator.cmake
@@ -3,9 +3,10 @@ include(FetchContent)
 FetchContent_Declare(
     jsonvalidator
     GIT_REPOSITORY https://github.com/pboettch/json-schema-validator.git
-    GIT_TAG        main
+    GIT_TAG        2.3.0
     GIT_PROGRESS TRUE
 ) 
 message("\nFetching json validator library")
 
 FetchContent_MakeAvailable(jsonvalidator)
+set_property(TARGET nlohmann_json_schema_validator PROPERTY CXX_STANDARD 20)
